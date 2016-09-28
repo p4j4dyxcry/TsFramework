0c029adbd6587ee2428c479ea359b5fb49091de4
yuki tsuneyama <at12k313@gmail.com>
2016-09-28 19:32:40 +0900

    During of light

--- TSFrameWork/TSFrameWork/Source/TsGfx/Light/TsLight.h
+++ TSFrameWork/TSFrameWork/Source/TsGfx/Light/TsLight.h
@@ -1,13 +1,35 @@
 #pragma once
 
 class TsLight :
-	public TsNameObject ,
+	public TsNameObject,
 	public ITsStaticNameObjectList<TsLight>,
 	public IHasTransform
 {
 public:
-	TsVector3 GetDir()const
+	enum LIGHT_TYPE
+	{
+		LIGHT_DIRECTIONAL,
+		LIGHT_POINT,
+		LIGHT_SPOT,
+	};
+
+	TsColor GetColor();
+	TsBool  SetColor( TsColor );
+	virtual LIGHT_TYPE GetLightType() = 0;
+protected:
+	TsColor m_color;
+	TsF32   m_intensity;
+};
+
+class TsDirectioalLight :public TsLight
+{
+public:
+	LIGHT_TYPE GetLightType()override
+	{
+		return LIGHT_DIRECTIONAL;
+	}
+	TsVector3 GetLightDir()
 	{
 		return TsVector3::up * m_pTransform->ToWorldMatrix();
-	};
+	}
 };
