56ec25962094e631fc527516ae61c1f8e646049d
yuki tsuneyama <at12k313@gmail.com>
2016-09-28 19:04:08 +0900

    Remove Light Transform & Add Intarface by HasTransform

--- TSFrameWork/TSFrameWork/Source/TsGfx/Light/TsLight.h
+++ TSFrameWork/TSFrameWork/Source/TsGfx/Light/TsLight.h
@@ -2,31 +2,12 @@
 
 class TsLight :
 	public TsNameObject ,
-	public ITsStaticNameObjectList<TsLight>
+	public ITsStaticNameObjectList<TsLight>,
+	public IHasTransform
 {
 public:
-	TsLight()
-	{
-		m_transform = TsNew( TsTransForm );
-	}
-	virtual ~TsLight()
-	{
-		TsSafeDelete( m_transform );
-	}
-	TsBool SetRotate( TsQuaternion& q )
-	{
-		m_transform->m_localRotate = q;
-	}
-	TsTransForm GetTransform()const
-	{
-		return *m_transform;
-	}
-
 	TsVector3 GetDir()const
 	{
-		return TsVector3::up * m_transform->ToWorldMatrix();
+		return TsVector3::up * m_pTransform->ToWorldMatrix();
 	};
-
-protected:
-	TsTransForm* m_transform;
 };
