a03c6c75cd4e06a403fbaf942211c88d67a7262a
yiki.tsuneyama <at12k313@gmail.com>
2016-09-29 23:04:24 +0900

    update resources

--- TSFrameWork/TSFrameWork/Resource/CopyShader.bat
+++ TSFrameWork/TSFrameWork/Resource/CopyShader.bat
@@ -1,7 +1,4 @@
-
-
-xcopy ShaderPass ..\..\bin\Resource\ShaderPass /Y 
-
+xcopy ShaderPackage ..\..\bin\Resource\ShaderPackage /Y 
 xcopy ShaderFlow ..\..\bin\Resource\ShaderFlow /Y 
-
+xcopy ShaderResource ..\..\bin\Resource\ShaderResource /Y 
 pause
