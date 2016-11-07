f4ec12b917428b5bf5361582eaad5ea3549da82d
yuki tsuneyama <at12k313@gmail.com>
2016-11-07 18:10:24 +0900

    Update TsColor

---
+++ TSFrameWork/TSFrameWork/Source/TsMath/TsColor.cpp
@@ -0,0 +1,76 @@
+#include "../../TsAfx.h"
+
+static const TsColor White  = TsColor(1, 1, 1, 1);
+static const TsColor Black  = TsColor(0, 0, 0, 1);
+static const TsColor Red    = TsColor(1, 0, 0, 1);
+static const TsColor Blue   = TsColor(0, 1, 0, 1);
+static const TsColor Green  = TsColor(0, 0, 1, 1);
+static const TsColor Gray   = TsColor(.5,.5,.5,1);
+
+TsColor::TsColor(const TsFloat4& color )
+{
+    *this = color;
+}
+
+TsColor::TsColor(const TsInt4& intColor)
+{
+    SetByInt(intColor);
+}
+
+TsColor::TsColor(TsUint byteCode)
+{
+    SetByByteCode(byteCode);
+}
+
+TsColor::TsColor(TsF32 r, TsF32 g, TsF32 b, TsF32 a)
+{
+    m_color = TsFloat4(r, g, b, a);
+}
+
+
+TsInt4 TsColor::ToRGBAInt()
+{
+    return TsInt4(m_color * 255.0f);
+}
+
+TsUint TsColor::ToByteCode()
+{
+    TsInt4 &&rgba = ToRGBAInt();
+    TsUint result = 0;
+    result |= (rgba.w << 6);
+    result |= (rgba.x << 4);
+    result |= (rgba.y << 2);
+    result |= (rgba.z);
+    return result ;
+}
+TsBool TsColor::SetByInt(TsInt4 color)
+{
+    m_color = (TsFloat4(color) / 255.0f);
+    return TS_TRUE;
+}
+TsBool TsColor::SetByByteCode(TsUint dw)
+{
+    TsInt4 rgba;
+    rgba.w = (dw & 0xff000000) >> 6;
+    rgba.x = (dw & 0x00ff0000) >> 4;
+    rgba.y = (dw & 0x0000ff00) >> 2;
+    rgba.z = (dw & 0x000000ff);
+    return SetByInt(rgba);
+}
+
+TsBool TsColor::IsAlpha()
+{
+    return m_color.w <= 1.0f;
+}
+
+TsColor& TsColor::operator = (const TsFloat4& f)
+{
+    m_color = f;
+    return *this;
+}
+
+TsColor& TsColor::operator = (const TsColor& f)
+{
+    m_color = f.m_color;
+    return *this;
+}
--- TSFrameWork/TSFrameWork/Source/TsMath/TsColor.h
+++ TSFrameWork/TSFrameWork/Source/TsMath/TsColor.h
@@ -3,26 +3,47 @@
 class TsColor
 {
 public:
-    TsInt4 ToRGBAInt()
-    {
-        return TsInt4( m_color * 255.0f );
-    }
-    TsBool SetByInt(TsInt4 color)
-    {
-        m_color = ( TsFloat4(color) / 255.0f);
-        return TS_TRUE;
-    }
-    TsBool IsAlpha()
-    {
-        return m_color.w <= 1.0f;
-    }
+    //===================================================
+    //! Constructor
+    //===================================================
+    TsColor(const TsFloat4& color = TsFloat4(1,1,1,1));
+    TsColor(const TsInt4& intColor);
+    TsColor(TsUint byteCode);
+    TsColor(TsF32 r, TsF32 g, TsF32 b, TsF32 a);
 
-    TsColor& operator = ( const TsFloat4& f )
-    {
-        m_color = f;
-        return *this;
-    }
-
+    //===================================================
+    //! public method
+    //===================================================
+    TsInt4 ToRGBAInt();
+    TsUint ToByteCode();
+    TsBool SetByInt(TsInt4 color);
+    TsBool SetByByteCode(TsUint dw);
+    TsBool IsAlpha();
+    TsColor& operator = (const TsFloat4& f);
+    TsColor& operator = (const TsColor&);
+    
+    //===================================================
+    //! Ref Of rgba
+    //===================================================
+    TsF32& r = m_color.x;
+    TsF32& g = m_color.y;
+    TsF32& b = m_color.z;
+    TsF32& a = m_color.w;
+public:
+    //===================================================
+    //! public propery
+    //===================================================
     TsFloat4 m_color;
+
+public:
+    //===================================================
+    //! public static propery
+    //===================================================
+    static const TsColor White ;
+    static const TsColor Black;
+    static const TsColor Red;
+    static const TsColor Blue;
+    static const TsColor Green;
+    static const TsColor Gray;
 
 };
